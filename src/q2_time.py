c436c49844847f823db29ff6e43ac0cecb693d45
sant-hoyos12 <132729697+sant-hoyos12@users.noreply.github.com>
2024-03-14 20:30:16 -0500

    Update q2_time.py

--- src/q2_time.py
+++ src/q2_time.py
@@ -2,14 +2,14 @@ from typing import List, Tuple
 import pandas as pd
 from emoji import emoji_list
 from collections import Counter
-#file_path = "farmers-protest-tweets-2021-2-4.json"
+file_path = "farmers-protest-tweets-2021-2-4.json"
 
 
 
 #@profile   #se puede descomentar para correr la función en la terminal con python -m memory_profiler q1_time.py y ver el uso de memoria.
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
     # Leer el archivo JSON en un DataFrame de Pandas
-    #df = pd.read_json(file_path, lines=True)
+    df = pd.read_json(file_path, lines=True)
     
     # Obtener un solo string que contenga todos los contenidos de los tweets
     single_string = df['content'].str.cat(sep=' ')
@@ -19,5 +19,3 @@ def q2_time(file_path: str) -> List[Tuple[str, int]]:
     
     # Contar los emojis y devolver los 10 más utilizados
     return Counter(emojis).most_common(10)
-
-print(q2_time(file_path))
